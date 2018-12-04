47dcfad4ae8c78ef127998b659859b50b3992826
krzysztofhejna <khejna15@wp.pl>
2018-12-04 18:18:42 +0100

    Update Javascript code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 (function(){ 
-    // Variables
+	// Variables
 	var modals = document.querySelectorAll('.modal');
-    var modalLinks = document.querySelectorAll('.show-modal');
-    var closeButtons = document.querySelectorAll('.modal .close');
+	var modalLinks = document.querySelectorAll('.show-modal');
+	var closeButtons = document.querySelectorAll('.modal .close');
 
-    // Functions
+	// Functions
 	var showModal = function(event){
 		var hrefToId = this.getAttribute("href").slice(1);
-        event.preventDefault();
-        disableModals();
-        document.getElementById(hrefToId).classList.add('show');
+		event.preventDefault();
+		disableModals();
+		document.getElementById(hrefToId).classList.add('show');
 		document.querySelector('#modal-overlay').classList.add('show');
 	};
 
@@ -18,24 +18,24 @@
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.remove('show');
 	};
-       
-    var disableModals = function() {
-        for (var i = 0; i < modals.length; i++){
-            modals[i].classList.remove('show');
-        }
-    }
 
-    // Event listeners
+	var disableModals = function() {
+		for (var i = 0; i < modals.length; i++){
+			modals[i].classList.remove('show');
+		}
+	}
+
+	// Event listeners
 	for(var i = 0; i < modalLinks.length; i++){
 		modalLinks[i].addEventListener('click', showModal);
 		closeButtons[i].addEventListener('click', hideModal);
 	}
-   
+
 	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
-		
+
 	for(var i = 0; i < modals.length; i++){
 		modals[i].addEventListener('click', function(event){
 			event.stopPropagation();
 		});
 	}
-})();      
+})();      	
